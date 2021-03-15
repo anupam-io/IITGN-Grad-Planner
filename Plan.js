5c675bb0d028bdfc96028178551a28888d5d2e1b
Anupam Kumar <akcgjc007@gmail.com>
2021-03-15 23:54:24 +0530

    updates

--- Plan.js
+++ Plan.js
@@ -1,11 +1,17 @@
+var assert = require('assert');
+
+class Semster{
+    
+}
+
 class Plan{
     constructor(){
-        this.successStatus = "0"
+        this.successStatus = "0",
+        this.sem = Semester[8]
     }
-    Semester Sem[8];
     
     setSemData(semNumber, courseList){
-        
+        assert(semNumber>=1 && semNumber<=8);
         // Add semester data to appropriate sem nunmber
     }
 
