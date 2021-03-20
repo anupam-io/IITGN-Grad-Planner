0656584bf77319f5d384ec9f990ffe73ccdfff26
Anupam Kumar <akcgjc007@gmail.com>
2021-03-20 21:29:55 +0530

    fixed footer + showing plans.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer class="bg-secondary page-footer font-small blue fixed">
-      <div class="footer-copyright text-center py-3">
+    <footer style={{
+      position: "fixed",
+      left: 0,
+      bottom: 0,
+      width: "100%",
+      color: "white"
+    }} className="bg-secondary page-footer font-small blue fixed">
+      <div className="footer-copyright text-center py-3">
         <b>For educational purposes only.</b>
         <br />
         Made by BobDaBuilder
